8535c94e0b171ba9c8a8bc4620fac94ff54e076f
zzzebbra <zzzebbra@yandex.ru>
2020-11-06 21:34:21 +0300

    Merge all blocks together

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,16 +2,16 @@ import Header from "../Header/Header";
 import Logo from "../Logo/Logo";
 import Footer from "../Footer/Footer";
 import About from "../About/About";
-import Player from './Player/Player';
+import Player from '../Player/Player';
 import "./main.css";
 
 function Main() {
   return (
     <section className="main">
-      <div className="wrapper">
+      {/* <div className="wrapper"> */}
         <Header />
         <Logo />
-      </div>
+      {/* </div> */}
       <Player />
       <About />
       <Footer />
